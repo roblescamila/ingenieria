65c10c726952bff70da6a3c0fa1df0e396a8e33c
mcselbor <roblescami@gmail.com>
2016-06-22 14:19:24 -0300

    Export agregado

--- src/GraphViz.java
+++ src/GraphViz.java
@@ -54,7 +54,7 @@ public class GraphViz
     /**
      * Load the config.properties file.
      */
-    private final static String cfgProp = "C:\\Users\\Cami\\Workspaces\\DGraph\\ingenieria\\config.properties";
+    private final static String cfgProp = "..\\config.properties";
     private final static Properties configFile = new Properties() {
         private final static long serialVersionUID = 1L; {
             try {
